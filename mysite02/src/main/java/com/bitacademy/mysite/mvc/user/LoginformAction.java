7eb4910830e12b12d791cc58b34a782d2e118661
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-23 12:54:44 +0900

    Action & Action Factory 적용 refactoring

---
+++ mysite02/src/main/java/com/bitacademy/mysite/mvc/user/LoginformAction.java
@@ -0,0 +1,19 @@
+package com.bitacademy.mysite.mvc.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bitacademy.web.mvc.Action;
+
+public class LoginformAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
+	}
+
+}
