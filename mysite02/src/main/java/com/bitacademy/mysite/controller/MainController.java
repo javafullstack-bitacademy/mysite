50596c6918defd9cfff99f8076ef50871cc7a65d
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-24 10:55:21 +0900

    servlet life circle & Listener & filter #1

--- mysite02/src/main/java/com/bitacademy/mysite/controller/MainController.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.bitacademy.mysite.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,17 +15,41 @@ import com.bitacademy.web.mvc.ActionFactory;
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	public void init() throws ServletException {
+		String configPath = getServletConfig().getInitParameter("config"); 
+		System.out.println("init() called:" + configPath);
+		super.init();
+	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		System.out.println("service() called");
+		super.service(arg0, arg1);
+	}
+
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet() called");
+
 		request.setCharacterEncoding("utf-8");
 		String actionName = request.getParameter("a");		
 
+		
 		ActionFactory actionFactory = new MainActionFactory();
 		Action action = actionFactory.getAction(actionName);
 		action.execute(request, response);
 	}
 
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
+	@Override
+	public void destroy() {
+		System.out.println("destroy() called!!!!!!!!!");
+		super.destroy();
+	}
+	
 }
