63ed92e94d780894d15c3ac3da0336c67eebdd79
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-24 15:10:42 +0900

    JSTL&EL 적용#1

--- mysite02/src/main/java/com/bitacademy/mysite/mvc/user/UpdateformAction.java
+++ mysite02/src/main/java/com/bitacademy/mysite/mvc/user/UpdateformAction.java
@@ -13,6 +13,11 @@ public class UpdateformAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 세션에서 authUser 가져오기
+		// 2. authUser에서 no 가져오기
+		// 3. no를 가지고 Repository를 통해 UserVo 가져오기
+		// 4. jsp로 UserVo 전달하면서 FORWARDING 하기
+		
 		WebUtil.forward(request, response, "/WEB-INF/views/user/updateform.jsp");
 	}
 
