71d9b3bb6a6b8bf550a8e4da42adf199c997cdd7
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-31 12:03:17 +0900

    서버 아이피 변경

--- mysite02/src/main/java/com/bitacademy/mysite/controller/MainController.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/MainController.java
@@ -30,6 +30,7 @@ public class MainController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		System.out.println("doGet() called");
 
 		String actionName = request.getParameter("a");		
