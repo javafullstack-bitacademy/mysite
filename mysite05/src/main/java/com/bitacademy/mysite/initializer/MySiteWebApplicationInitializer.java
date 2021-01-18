4fe2369ba9401e981e01cc1eaca7128c5f66e2ae
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2021-01-18 10:02:53 +0900

    mysite05 #4

--- mysite05/src/main/java/com/bitacademy/mysite/initializer/MySiteWebApplicationInitializer.java
+++ mysite05/src/main/java/com/bitacademy/mysite/initializer/MySiteWebApplicationInitializer.java
@@ -1,6 +1,9 @@
 package com.bitacademy.mysite.initializer;
 
+import javax.servlet.Filter;
+
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -37,5 +40,10 @@ public class MySiteWebApplicationInitializer extends AbstractAnnotationConfigDis
 		diapatcherServlet.setThrowExceptionIfNoHandlerFound(true);
 		
 		return diapatcherServlet;
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] { new CharacterEncodingFilter("UTF-8", true) };
 	}
 }
